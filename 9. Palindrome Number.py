014597bdbc3fb4011e89214a19794a0ef23c6148
Brent <93208785+BMarshyyy@users.noreply.github.com>
2022-11-25 10:13:45 -0600

    Update 9. Palindrome Number.py

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -1,3 +1,6 @@
+# Runtime: 117 ms, faster than 46.30% of Python online submissions for Palindrome Number.
+# Memory Usage: 13.5 MB, less than 11.58% of Python online submissions for Palindrome Number.
+
 def isPalindrome(x):
         x_rev = reversed(str(x))
         x = str(x)
