34623e3c8f1dbc40b54669511998c5396ae06a85
Brent <93208785+BMarshyyy@users.noreply.github.com>
2022-11-25 10:16:13 -0600

    Update 13. Roman to Integer.py

--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -1,3 +1,6 @@
+# Runtime: 23 ms, faster than 99.73% of Python online submissions for Roman to Integer.
+# Memory Usage: 13.8 MB, less than 82.18% of Python online submissions for Roman to Integer.
+
 def romanToInt(text):
       num_dict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
       reduction_dict = {"IV":2, "IX":2, "XL":20, "XC":20, "CD":200, "CM":200}
